b7c25fef2480ee2de8ba1e4656b43a7fe2f70bfd
Sergey Lebedev <lebedev2k@mail.ru>
2023-12-11 17:06:17 +0700

    warmpup problem A solved

--- 00 warmup A.py
+++ 00 warmup A.py
@@ -11,13 +11,10 @@ a = list(map(int, lines[1].split()))
 for i in range(2, len(lines)):
     L, R = list(map(int, lines[i].split()))
     result = 'NOT FOUND'
-    if R-L > 1:
-        min_value = min(a[L:R+1])
-        for item in a[L:R+1]:
-            if item != min_value:
-                result = item
-                break
-    else:
-        if a[L] != a[R]:
-            result = a[L] < a[R] if a[R] else a[L]
+    min_value = min(a[L:R + 1])
+    for item in a[L:R + 1]:
+        if item != min_value:
+            result = item
+            break
     print(result)
+

d5782c338d20f9cd22cb15a1f47d5e4e3b5e065f
lebedev2k <lebedev2k@mail.ru>
2023-12-24 15:29:30 +0700

    warmup E done

--- 00 warmup E.py
+++ 00 warmup E.py
@@ -1,18 +1,25 @@
-#https://contest.yandex.ru/contest/53027/problems/E/
-#E. Средний уровень
+# https://contest.yandex.ru/contest/53027/problems/E/
+# E. Средний уровень
 
-#засыпался на тестах по превышению времени выполнения
+# засыпался на тестах по превышению времени выполнения
 from sys import stdin
+from itertools import accumulate
 
 lines = []
 for line in stdin:
     lines.append(line.rstrip())
 
 scores = list(map(int, lines[1].split()))
-rates = []
-for score in scores:
-    summ = 0
-    for item in scores:
-        summ += abs(item-score)
-    rates.append(str(summ))
+accum_scores = list(accumulate(scores, initial=0))
+n = len(scores)
+rates = [str(i * scores[i] - accum_scores[i] + accum_scores[-1] - accum_scores[i + 1] - (n - i - 1) * scores[i])
+         for i in range(n)]
 print(' '.join(rates))
+
+# 1 solution, bug: low speed
+# for score in scores:
+#     summ = 0
+#     for item in scores:
+#         summ += abs(item-score)
+#     rates.append(str(summ))
+
